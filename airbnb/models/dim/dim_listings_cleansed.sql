fe44056ac1758c81d92dc4e94de4c62cc19e835a
laly <elisitajulia2403@gmail.com>
2026-07-21 15:54:37 -0400

    segundo commit sin modificacion del id- review---igual que el profesor

--- airbnb/models/dim/dim_listings_cleansed.sql
+++ airbnb/models/dim/dim_listings_cleansed.sql
@@ -4,7 +4,7 @@ WITH  src_listing AS (
 SELECT 
     listing_id,
     listing_name,
-    room_type,
+    room_type AS ROOM_TYPE,
     CASE WHEN MINIMUM_NIGHTS = 0 THEN  1
     ELSE COALESCE(MINIMUM_NIGHTS, 1)
     END AS MINIMUM_NIGHTS,
@@ -12,4 +12,5 @@ SELECT
     REPLACE( PRICE_STR, '$'):: NUMBER (10,2) AS PRICE,
     CREATED_AT,
     UPDATED_AT
+
 FROM  SRC_LISTINGS
