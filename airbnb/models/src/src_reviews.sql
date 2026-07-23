af4b59a686746e8bfe8cda185815932fb138221a
laly <elisitajulia2403@gmail.com>
2026-07-23 13:52:40 -0400

    Revierto review_id para continuar ejercicios del profesor

--- airbnb/models/src/src_reviews.sql
+++ airbnb/models/src/src_reviews.sql
@@ -2,7 +2,6 @@ WITH raw_reviews AS (
       select * from  airbnb.raw.raw_reviews
 )
 SELECT 
-{{ dbt_utils.generate_surrogate_key(['listing_id','date','reviewer_name'])}}  as review_id,
  listing_id,
  date as review_date,
  reviewer_name,
